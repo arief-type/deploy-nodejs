40475786213e745d68ac65a8da376f9215f34d89
arief-type <msyaifullahalarief91@gmail.com>
2024-08-06 12:40:55 +0700

    test lagi 4

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import db from "@/utils/database";
 import routes from "@/routes";
 import bodyParser from "body-parser";
 import './utils/module-alias'
+import path from "path";
 
 const app = express();
 const PORT = process.env.APP_PORT || 3000;
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", routes);
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
